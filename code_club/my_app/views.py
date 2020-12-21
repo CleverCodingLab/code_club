dae47eec11b4c67ebc0fddf10cbc3fd1619b74c6
LEGEND <baralprem4@gmail.com>
2020-12-21 19:09:13 +0530

    update 12/21/2020

--- code_club/my_app/views.py
+++ code_club/my_app/views.py
@@ -1,9 +1,14 @@
 import requests
-from django.shortcuts import render
 from bs4 import BeautifulSoup
-
+from urllib.parse import quote_plus
+# from requests.compat import quote_plus
+from django.shortcuts import render
+from . import models
 
 # Create your views here.
+
+BASE_CRAIGLIST_URL = 'https://delhi.craigslist.org/d/services/search/bbb?query={}'
+
 
 def home(request):
     return render(request, 'base.html')
@@ -11,6 +16,15 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    # print(quote_plus(search))
+    final_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
+    print(final_url)
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_titles = soup.find_all('a', {'class': 'result-title'})
+    print(post_titles)
     stuff_for_frontend = {
         'search': search,
     }
